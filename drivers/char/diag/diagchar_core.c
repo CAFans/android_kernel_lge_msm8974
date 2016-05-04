e875eb02c635a190845fe1c4eb36e6f69f50bdcc
Kendrick Vargas <ken@hudat.com>
2016-05-04 16:01:11 -0400

    Import LGE non-8992 specific diag/ddm tty code
    From H901 v20e sources

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -1609,8 +1609,15 @@ static ssize_t diagchar_write(struct file *file, const char __user *buf,
 	uint8_t retry_count = 0;
 	uint8_t max_retries = 3;
 #ifdef DIAG_DEBUG
+#ifndef CONFIG_LGE_USB_G_ANDROID
 	int length = 0, i;
 #endif
+#endif
+
+#ifdef CONFIG_LGE_DM_APP
+    char *buf_cmp;
+#endif
+
 	struct diag_send_desc_type send = { NULL, NULL, DIAG_STATE_START, 0 };
 	struct diag_hdlc_dest_type enc = { NULL, NULL, 0 };
 	void *buf_copy = NULL;
@@ -1646,6 +1653,16 @@ static ssize_t diagchar_write(struct file *file, const char __user *buf,
 		return -EIO;
 	}
 #endif /* DIAG over USB */
+
+#ifdef CONFIG_LGE_DM_APP
+    if (driver->logging_mode == DM_APP_MODE) {
+        /* only diag cmd #250 for supporting testmode tool */
+        buf_cmp = (char *)buf + 4;
+        if (*(buf_cmp) != 0xFA)
+            return 0;
+    }
+#endif
+
 	if (pkt_type == DCI_DATA_TYPE) {
 		user_space_data = diagmem_alloc(driver, payload_size,
 								POOL_TYPE_USER);
@@ -1756,9 +1773,14 @@ static ssize_t diagchar_write(struct file *file, const char __user *buf,
 		buf = buf + 4;
 #ifdef DIAG_DEBUG
 		pr_debug("diag: user space data %d\n", payload_size);
+#ifdef CONFIG_LGE_USB_G_ANDROID
+		print_hex_dump(KERN_DEBUG, "user: ", 16, 1, DUMP_PREFIX_ADDRESS,
+				driver->user_space_data_buf + token_offset, payload_size, 1);
+#else
 		for (i = 0; i < payload_size; i++)
 			pr_debug("\t %x", *((driver->user_space_data_buf
 						+ token_offset)+i));
+#endif
 #endif
 		err = diag_process_userspace_remote(remote_proc,
 						    driver->user_space_data_buf
@@ -1844,9 +1866,14 @@ static ssize_t diagchar_write(struct file *file, const char __user *buf,
 
 #ifdef DIAG_DEBUG
 	printk(KERN_DEBUG "data is -->\n");
+#ifdef CONFIG_LGE_USB_G_ANDROID
+	print_hex_dump(KERN_DEBUG, "data: ", 16, 1, DUMP_PREFIX_ADDRESS,
+			(unsigned char *)buf_copy, payload_size, 1);
+#else
 	for (i = 0; i < payload_size; i++)
 		printk(KERN_DEBUG "\t %x \t", *(((unsigned char *)buf_copy)+i));
 #endif
+#endif /* DIAG DEBUG */
 	send.state = DIAG_STATE_START;
 	send.pkt = buf_copy;
 	send.last = (void *)(buf_copy + payload_size - 1);
@@ -1854,6 +1881,7 @@ static ssize_t diagchar_write(struct file *file, const char __user *buf,
 #ifdef DIAG_DEBUG
 	pr_debug("diag: Already used bytes in buffer %d, and"
 	" incoming payload size is %d\n", driver->used, payload_size);
+#ifndef CONFIG_LGE_USB_G_ANDROID
 	printk(KERN_DEBUG "hdlc encoded data is -->\n");
 	for (i = 0; i < payload_size + 8; i++) {
 		printk(KERN_DEBUG "\t %x \t", *(((unsigned char *)buf_hdlc)+i));
@@ -1861,6 +1889,7 @@ static ssize_t diagchar_write(struct file *file, const char __user *buf,
 			length++;
 	}
 #endif
+#endif /* DIAG DEBUG */
 	mutex_lock(&driver->diagchar_mutex);
 	if (!buf_hdlc)
 		buf_hdlc = diagmem_alloc(driver, HDLC_OUT_BUF_SIZE,
@@ -2249,8 +2278,13 @@ static int diagchar_setup_cdev(dev_t devno)
 		return -1;
 	}
 
+#ifdef CONFIG_MACH_LGE
+	driver->diag_dev = device_create(driver->diagchar_class, NULL, devno,
+					 (void *)driver, "diag_lge");
+#else
 	driver->diag_dev = device_create(driver->diagchar_class, NULL, devno,
 					 (void *)driver, "diag");
+#endif
 
 	if (!driver->diag_dev)
 		return -EIO;
--- drivers/char/diag/diagfwd.c
+++ drivers/char/diag/diagfwd.c
@@ -51,6 +51,14 @@
 
 #define SMD_DRAIN_BUF_SIZE 4096
 
+#ifdef CONFIG_LGE_DM_APP
+#include "lg_dm_tty.h"
+#endif
+
+#ifdef CONFIG_LGE_ACG_CARRIER_CODE
+#include "diag_acg.h"
+#endif
+
 int diag_debug_buf_idx;
 unsigned char diag_debug_buf[1024];
 /* Number of entries in table of buffers */
@@ -693,6 +701,21 @@ void diag_smd_send_req(struct diag_smd_info *smd_info)
 		(driver->logging_mode == MEMORY_DEVICE_MODE)) {
 			chk_logging_wakeup();
 	}
+
+#ifdef CONFIG_LGE_DM_APP
+    else if (smd_info->ch && (driver->logging_mode == DM_APP_MODE)) {
+        if( buf != NULL && smd_info->in_busy_1 == 0){
+            smd_info->in_busy_1 = 1;
+        }
+        else if(buf != NULL && smd_info->in_busy_2 == 0){
+            smd_info->in_busy_2 = 1;
+        }
+
+        lge_dm_tty->set_logging = 1;
+        wake_up_interruptible(&lge_dm_tty->waitq);
+    }
+#endif
+
 	return;
 
 fail_return:
@@ -751,6 +774,15 @@ void encode_rsp_and_send(int buf_length)
 		 */
 		if (driver->logging_mode == MEMORY_DEVICE_MODE)
 			chk_logging_wakeup();
+
+#ifdef CONFIG_LGE_DM_APP
+        if (driver->logging_mode == DM_APP_MODE)
+        {
+            lge_dm_tty->set_logging = 1;
+            wake_up_interruptible(&lge_dm_tty->waitq);
+        }
+#endif
+
 	}
 	if (driver->rsp_buf_busy) {
 		pr_err("diag: unable to get hold of response buffer\n");
@@ -1067,6 +1099,40 @@ int diag_check_common_cmd(struct diag_pkt_header_t *header)
 	return 0;
 }
 
+#if defined(CONFIG_LGE_USB_DIAG_LOCK)
+extern int get_diag_enable(void);
+#define DIAG_ENABLE			1
+#define DIAG_DISABLE			0
+#define COMMAND_PORT_LOCK		0xA1
+#define COMMAND_WEB_DOWNLOAD		0xEF
+#define COMMAND_ASYNC_HDLC_FLAG		0x7E
+#define COMMAND_DLOAD_RESET		0x3A
+#define COMMAND_TEST_MODE		0xFA
+#define COMMAND_TEST_MODE_RESET		0x29
+
+int is_filtering_command(char *buf)
+{
+	if (buf == NULL)
+		return 0;
+
+	switch(buf[0]) {
+		case COMMAND_PORT_LOCK :
+#if !defined(CONFIG_LGE_USB_DIAG_LOCK_SPR)
+		case COMMAND_WEB_DOWNLOAD :
+		case COMMAND_ASYNC_HDLC_FLAG :
+		case COMMAND_DLOAD_RESET :
+		case COMMAND_TEST_MODE :
+		case COMMAND_TEST_MODE_RESET :
+#endif
+			return 1;
+		default:
+			break;
+	}
+
+	return 0;
+}
+#endif
+
 int diag_process_apps_pkt(unsigned char *buf, int len)
 {
 	uint16_t subsys_cmd_code;
@@ -1077,6 +1143,16 @@ int diag_process_apps_pkt(unsigned char *buf, int len)
 	int mask_ret;
 	int status = 0;
 	int write_len = 0;
+#ifdef CONFIG_LGE_ACG_CARRIER_CODE
+	unsigned long int carrier_code = 0;
+	int result = 0;
+#endif
+
+#if defined(CONFIG_LGE_USB_DIAG_LOCK)
+	/* buf[0] : 0xA1(161) is a diag command for mdm port lock */
+ if (!is_filtering_command(buf) && (get_diag_enable() == DIAG_DISABLE))
+		return 0;
+#endif
 
 	/* Check if the command is a supported mask command */
 	mask_ret = diag_process_apps_masks(buf, len);
@@ -1206,6 +1282,17 @@ int diag_process_apps_pkt(unsigned char *buf, int len)
 		encode_rsp_and_send(5);
 		return 0;
 	}
+#ifdef CONFIG_LGE_ACG_CARRIER_CODE
+	else if  ((*buf == 0x26) && (*(buf+1) == 0x6d) && (*(buf+2) == 0x84))
+	{
+		carrier_code = get_carrier_code();
+	}
+	else if  ((*buf == 0x27) && (*(buf+1) == 0x6d) && (*(buf+2) == 0x84))
+	{
+		carrier_code = 10*(*(buf+4))+(*(buf+3));
+		result = set_carrier_code(carrier_code);
+	}
+#endif
 	/* Log on Demand Rsp */
 	else if (*buf == DIAG_CMD_LOG_ON_DMND) {
 		write_len = diag_cmd_log_on_demand(buf, len,
@@ -1363,10 +1450,16 @@ void diag_process_hdlc(void *data, unsigned len)
 	}
 
 #ifdef DIAG_DEBUG
+#ifdef CONFIG_LGE_USB_G_ANDROID
+	pr_debug("diag: hdlc.dest_idx = %d\n", hdlc.dest_idx);
+	print_hex_dump(KERN_DEBUG, "process: ", 16, 1, DUMP_PREFIX_ADDRESS,
+			(unsigned char *)driver->hdlc_buf, hdlc.dest_idx, 1);
+#else
 	pr_debug("diag: hdlc.dest_idx = %d", hdlc.dest_idx);
 	for (i = 0; i < hdlc.dest_idx; i++)
 		printk(KERN_DEBUG "\t%x", *(((unsigned char *)
 							driver->hdlc_buf)+i));
+#endif
 #endif /* DIAG DEBUG */
 	/* ignore 2 bytes for CRC, one for 7E and send */
 	if ((driver->smd_data[MODEM_DATA].ch) && (ret) && (type) &&
@@ -1449,6 +1542,17 @@ static int diagfwd_mux_open(int id, int mode)
 	int i;
 	unsigned long flags;
 
+#ifdef CONFIG_LGE_DM_APP
+    if (driver->logging_mode == DM_APP_MODE) {
+        if (mode == DIAG_USB_MODE) {
+            printk(KERN_DEBUG "diag: diagfwd_mux_open USB connected in DM_APP_MODE\n");
+            driver->usb_connected = 1;
+        }
+
+        return 0;
+    }
+#endif
+
 	if (driver->rsp_buf_busy) {
 		/*
 		 * When a client switches from callback mode to USB mode
@@ -1495,6 +1599,17 @@ static int diagfwd_mux_close(int id, int mode)
 	unsigned long flags;
 	struct diag_smd_info *smd_info = NULL;
 
+#ifdef CONFIG_LGE_DM_APP
+    if (driver->logging_mode == DM_APP_MODE) {
+        if (mode == DIAG_USB_MODE) {
+            printk(KERN_DEBUG "diag: diagfwd_mux_close USB disconnected in DM_APP_MODE\n");
+            driver->usb_connected = 0;
+        }
+
+        return 0;
+    }
+#endif
+
 	switch (mode) {
 	case DIAG_USB_MODE:
 		driver->usb_connected = 0;
@@ -1538,6 +1653,7 @@ static int diagfwd_mux_read_done(unsigned char *buf, int len, int ctxt)
 
 	diag_process_hdlc(buf, len);
 	diag_mux_queue_read(ctxt);
+
 	return 0;
 }
 
@@ -1557,11 +1673,24 @@ static int diagfwd_mux_write_done(unsigned char *buf, int len, int buf_ctxt,
 	type = GET_BUF_TYPE(buf_ctxt);
 	num = GET_BUF_NUM(buf_ctxt);
 
+#ifdef CONFIG_LGE_DM_APP
+    if (driver->logging_mode == DM_APP_MODE)
+        driver->rsp_buf_busy = 0;
+#endif
+
 	switch (type) {
 	case SMD_DATA_TYPE:
 		if (peripheral >= 0 && peripheral < NUM_SMD_DATA_CHANNELS) {
 			smd_info = &driver->smd_data[peripheral];
 			diag_smd_reset_buf(smd_info, num);
+
+#ifdef CONFIG_LGE_DM_APP
+            if (driver->logging_mode == DM_APP_MODE) {
+                flush_workqueue(smd_info->wq);
+                wake_up(&driver->smd_wait_q);
+            }
+#endif
+
 		} else if (peripheral == APPS_DATA) {
 			diagmem_free(driver, (unsigned char *)buf,
 				     POOL_TYPE_HDLC);
@@ -1648,6 +1777,14 @@ void diag_smd_notify(void *ctxt, unsigned event)
 		     driver->logging_mode == MEMORY_DEVICE_MODE)) {
 			diag_ws_on_notify();
 		}
+
+#ifdef CONFIG_LGE_DM_APP
+        else if (smd_info->type == SMD_DATA_TYPE &&
+            driver->logging_mode == DM_APP_MODE) {
+            diag_ws_on_notify();
+        }
+#endif
+
 	}
 
 	wake_up(&driver->smd_wait_q);
